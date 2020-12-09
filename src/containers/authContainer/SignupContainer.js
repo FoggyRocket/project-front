a14002f3a2de5e6795c726c29dccda2edf53bf3b
Dylan Torres <foggyrocket@MacBook-Pro-de-Dylan.local>
2020-12-08 21:58:40 -0600

    Signup and ws

--- src/containers/authContainer/SignupContainer.js
+++ src/containers/authContainer/SignupContainer.js
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
-import { login } from "../../services/userWs";
-import { Link } from 'react-router-dom';
+import { signup } from "../../services/userWs";
+import {Link} from "react-router-dom"
 
 
-export default class AuthContainer extends Component {
+export default class SignupContainer extends Component {
     //state === minibase de datos para la clase e hijos en caso de que los herede (o se los pase)
     state = {
       data:{}
@@ -26,14 +26,18 @@ export default class AuthContainer extends Component {
     onSubmit = (event) => {
         event.preventDefault()
         console.log("Voy a enviar datos")
-        login(this.state.data).then((response)=>{
+        //voy a destructurar los pros
+        const { history } = this.props;
+
+        signup(this.state.data).then((response)=>{
             //aqui andetro si todo sale bien 
             //monstramos un mensaje y mandamos a la siguiente seccion
             this.setState({ data:{}})
             console.log("Felicidades",response)
+            history.push("/login")
 
         }).catch((error)=>{
-            console.log("hay un error",error)
+            console.log("hay un error",error.response)
         })
 
         
@@ -47,7 +51,7 @@ export default class AuthContainer extends Component {
             <section className="uk-section">
                 <div className="uk-container uk-flex uk-flex-center">
                     <div className="uk-width-1-4">
-                        <h3>Bienveido </h3>
+                        <h3>Registrate </h3>
                         <form
                         // metodo={(event)=>onSumit(event)}
                             onSubmit={onSubmit}
@@ -69,6 +73,20 @@ export default class AuthContainer extends Component {
 
                             <div className="uk-margin">
                                 <div className="uk-inline">
+                                    <span className="uk-form-icon" uk-icon="icon: user"></span>
+                                    <input 
+                                        className="uk-input"
+                                        type="text"
+                                        name="name"
+                                        onChange={handleChange} 
+                                        required
+                                        value={data["name"] ? data["name"] : ""}    
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="uk-margin">
+                                <div className="uk-inline">
                                     <span className="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
                                     <input 
                                         className="uk-input" 
@@ -80,14 +98,30 @@ export default class AuthContainer extends Component {
                                     />
                                 </div>
                             </div>
+
+                            <div className="uk-margin">
+                                <div className="uk-inline">
+                                    <span className="uk-form-icon" uk-icon="icon: lock"></span>
+                                    <input 
+                                        className="uk-input"
+                                        type="password"
+                                        name="confirmPassword"
+                                        onChange={handleChange} 
+                                        required
+                                        value={data["confirmPassword"] ? data["confirmPassword"] : ""}    
+                                    />
+                                </div>
+                            </div>
+
                             <div className="uk-text-meta">
-                                Aún no tienes cuenta?{" "}
-                                <Link className="uk-text-primary" to="/signup" >
-                                    Create una cuenta Morr@
+                                Ya tienes cuenta?{" "}
+                                <Link className="uk-text-primary" to="/login" >
+                                    Ingresa morr@
                                 </Link>
                             </div>
+
                             <button className="uk-button uk-button-primary">
-                                Entrar
+                                Registrate
                             </button>
                         </form>
                     </div>
